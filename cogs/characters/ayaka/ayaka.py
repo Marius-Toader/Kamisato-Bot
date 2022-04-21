edaa74029d2267948453ff8aca436713e15c802e
admin <ultratiger95@gmail.com>
2022-04-21 18:55:33 +0200

    First iteration of Character cards

---
+++ cogs/characters/ayaka/ayaka.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+import discord
+import json
+import requests
+
+class Ayaka(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+    @commands.command(name="ayaka")
+    async def ayaka(self, ctx: commands.Context):
+        response = requests.get('https://api.genshin.dev/characters/ayaka')
+        data = response.text
+        JSONData = json.loads(data)
+        
+        embed = discord.Embed(title=JSONData["name"])
+        embed.set_thumbnail(url="https://github.com/Marius-Toader/Kamisato-Bot/tree/dev/assets/ayaka.jpg")
+        embed.add_field(name="Description", value=JSONData["description"], inline=False)
+        embed.add_field(name="Nation", value=JSONData["nation"], inline=True)
+        embed.add_field(name="Vision", value=JSONData["vision"], inline=True)
+        embed.add_field(name="Weapon", value=JSONData["weapon"], inline=True)
+        embed.add_field(name="Affiliation", value=JSONData["affiliation"], inline=True)
+        embed.add_field(name="Rarity", value=str(JSONData["rarity"]) + " ★", inline=True)
+        embed.add_field(name="Constellation", value=JSONData["constellation"], inline=True)
+        await ctx.send(embed=embed)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Ayaka(bot))
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ bot = commands.Bot(command_prefix = "%")
 
 bot.load_extension("hello")
 bot.load_extension("cogs.music.music")
+bot.load_extension("cogs.characters.ayaka.ayaka")
 
 bot.run(token)
